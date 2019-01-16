f21a08620539c752a7ee49464c9dc10a1afda922
MikeKlement <smallman@hotmail.de>
2019-01-16 16:54:33 +0100

    api code

---
+++ MS3/Client/APIs/youtube/main/java/com/example/agando/youtubeplayer/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.agando.youtubeplayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+public class MainActivity extends YouTubeBaseActivity {
+
+    YouTubePlayerView mYoutubePlayerview;
+    Button btnPlay;
+    YouTubePlayer.OnInitializedListener mOnInitializedListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mYoutubePlayerview = findViewById(R.id.youtubePlay);
+        btnPlay = findViewById(R.id.btnPlay);
+
+        mOnInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+
+                youTubePlayer.loadVideo("ZNrBe7f6JEg");
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mYoutubePlayerview.initialize(YoutubeConfig.getApiKey(), mOnInitializedListener);
+            }
+        });
+    }
+}
---
+++ MS3/Client/APIs/youtube/main/java/com/example/agando/youtubeplayer/YoutubeConfig.java
@@ -0,0 +1,12 @@
+package com.example.agando.youtubeplayer;
+
+public class YoutubeConfig {
+    public YoutubeConfig(){
+    }
+    private static final String API_KEY = "AIzaSyBIV7CEmR6D7CqgNZjDwAZQjMHvRMS6QAk";
+
+    public static String getApiKey(){
+        return API_KEY;
+    }
+}
+
